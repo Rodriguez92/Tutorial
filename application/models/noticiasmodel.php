6edcbdeeacc05063c98dde5d8251db234548adf0
Diego I. Rodriguez <ing.diegorod@gmail.com>
2015-02-17 15:44:11 -0600

    añadi los assets de bootstrap y me quede trabajando con los formularios para agregar noticias

---
+++ application/models/noticiasmodel.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class NoticiasModel extends CI_Model {
+
+	public function __construct()
+	{
+		$this->load->database();
+	}
+
+	public function obtener_noticias($value='')
+	{
+		# code...
+	}
+
+	public function insertar_noticias($value='')
+	{
+		# code...
+	}
+
+}
+
+/* End of file noticiasModel.php */
+/* Location: ./application/models/noticiasModel.php */
